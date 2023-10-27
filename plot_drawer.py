5e2e7a3d914b1b1b5219bf336ed155ab1b43b4a6
spenc <spencerzhyp@gmail.com>
2023-10-27 11:54:34 -0700

    optimized plot_drawer.py

--- plot_drawer.py
+++ plot_drawer.py
@@ -16,35 +16,32 @@ def extract_time(comm_list, number):
     my_sort = re.search(pattern, comm_list[7])
     q_sort_time = float(q_sort.group(1)) if q_sort else None
     my_sort_time = float(my_sort.group(1)) if my_sort else None
-    print(number, q_sort_time, my_sort_time)
     return number, q_sort_time, my_sort_time
 
 
 def time_list():
-    result = []
-    for i in num_size:
-        command = "./quick-sort " + ("%s" % i)
-        time = extract_time(get_command(command), i - 10)
-        result.append(time)
-    return result
-
-
-def plot(time_comp_list):
     x = []
     q_sort_y = []
     my_sort_y = []
-    for time_tuple in time_comp_list:
-        x.append(time_tuple[0] / 10000)
-        q_sort_y.append(time_tuple[1])
-        my_sort_y.append(time_tuple[2])
+    for i in num_size:
+        command = "./quick-sort " + ("%s" % i)
+        time = extract_time(get_command(command), i - 10)
+        x.append(time[0] / 1000)
+        q_sort_y.append(time[1])
+        my_sort_y.append(time[2])
+        print(time)
     q_sort_y_smooth = gaussian_filter1d(q_sort_y, sigma=5)
     my_sort_y_smooth = gaussian_filter1d(my_sort_y, sigma=5)
+    return x, q_sort_y_smooth, my_sort_y_smooth
+
+
+def plot(x, y1, y2):
     plt.title('Comparison of Quicksort and Random Quicksort')
     plt.xlabel('Size of input array(10k)')
     plt.ylabel('Time(ms)')
     plt.grid(True)
-    plt.plot(x,q_sort_y_smooth, label = 'Quicksort')
-    plt.plot(x,my_sort_y_smooth, label = 'Random Quicksort')
+    plt.plot(x, y1, label='Quicksort')
+    plt.plot(x, y2, label='Random Quicksort')
     plt.legend()
     plt.ticklabel_format(style='plain')
 
@@ -52,5 +49,5 @@ def plot(time_comp_list):
 
 
 if __name__ == '__main__':
-    times = time_list()
-    plot(times)
+    coordinate = time_list()
+    plot(coordinate[0], coordinate[1], coordinate[2])
